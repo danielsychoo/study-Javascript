225d8450516abba3f36f00de6423bb1fce184500
danielsychoo <choointern@gmail.com>
2021-04-06 15:28:44 +0900

    Update velopert react 18

--- TIL/2021-04-05/begin-react/src/App.js
+++ TIL/2021-04-05/begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState, useRef, useMemo, useCallback } from 'react';
 
 import CreateUser from './CreateUser';
 import UserList from './UserList';
@@ -13,15 +13,18 @@ function App() {
   // * state destructuring
   const { username, email } = inputs;
 
-  const onChange = e => {
-    // 각 e.target을 unique하게 사용하기 위해
-    // 물론 이것도 destructuring
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const onChange = useCallback(
+    e => {
+        // 각 e.target을 unique하게 사용하기 위해
+        // 물론 이것도 destructuring
+        const { name, value } = e.target;
+        setInputs({
+          ...inputs,
+          [name]: value,
+        });
+      },
+      [inputs]
+  );
 
   // initial user data
   const [users, setUsers] = useState([
@@ -47,48 +50,61 @@ function App() {
 
   // id를 useRef로 관리 initialValue는 4
   const nextId = useRef(4);
-  const onCreate = () => {
-    // setState에 바로 넣으면 복잡하니께 따로 객체로
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    }
-    // 준비된 user객체 setState 이전 data는 spread
-    setUsers([
-      ...users,
-      user
-    ])
-    // onClick 후 input value를 공백으로 만드는 부분
-    setInputs({
-      username: '',
-      email: '',
-    });
-
-    // id를 계속 ++ 되게 해줌
-    // map으로 돌릴때 unique key이용해야 하니께
-    nextId.current++ ;
-  }
+  const onCreate = useCallback(
+    () => {
+      // setState에 바로 넣으면 복잡하니께 따로 객체로
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      }
+      // 준비된 user객체 setState 이전 data는 spread
+      setUsers([
+        ...users,
+        user
+      ])
+      // onClick 후 input value를 공백으로 만드는 부분
+      setInputs({
+        username: '',
+        email: '',
+      });
+  
+      // id를 계속 ++ 되게 해줌
+      // map으로 돌릴때 unique key이용해야 하니께
+      nextId.current++ ;
+    },
+    [users, username, email]
+  );
 
   // parameter로 받은 id의 user를 제외한 user만 filtering하는
   // 방식이 얕은 복사로 setState하는 것과 같다.
-  const onRemove = id => {
-    setUsers(users.filter(user => user.id !== id));
-  };
+  const onRemove = useCallback(
+    id => {
+      setUsers(users.filter(user => user.id !== id));
+    },
+    [users]
+  );
+  
+
 
   // map으로 돌면서 click된 id가 동일한 것만 active 반전
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-        user.id === id
-          ? {
-              ...user,
-              active: !user.active,
-            }
-          : user
-      )
-    );
-  };
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+          user.id === id
+            ? {
+                ...user,
+                active: !user.active,
+              }
+            : user
+        )
+      );
+    },
+    [users]
+  );
+  
+
 
   // 활성 사용자를 세는 함수
   const countActiveUsers = users => {
