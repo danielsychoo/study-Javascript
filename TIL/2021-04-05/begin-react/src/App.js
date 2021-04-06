244128ee17e7044bcc25453e90c007995477eae3
danielsychoo <choointern@gmail.com>
2021-04-06 18:33:37 +0900

    Update velopert react 21

--- TIL/2021-04-05/begin-react/src/App.js
+++ TIL/2021-04-05/begin-react/src/App.js
@@ -1,86 +1,80 @@
-import React, { useReducer, useRef, useMemo, useCallback } from 'react';
+import React, { useReducer, useRef, useMemo, useCallback } from "react";
 
-import CreateUser from './CreateUser';
-import UserList from './UserList';
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
 
-import useInputs from './Hooks/useInputs';
+import useInputs from "./Hooks/useInputs";
 
+// 활성 사용자를 세는 함수
+const countActiveUsers = (users) => {
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter((user) => user.active).length;
+};
 
-  // 활성 사용자를 세는 함수
-  const countActiveUsers = users => {
-    console.log('활성 사용자 수를 세는중...');
-    return users.filter(user => user.active).length;
-  };
+// initial user data
+const initialState = {
+  // inputs: {
+  //   username: '',
+  //   email: '',
+  // },
+  users: [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ],
+};
 
-  // initial user data
-  const initialState = {
-    // inputs: {
-    //   username: '',
-    //   email: '',
-    // },
-    users: [
-      {
-        id: 1,
-        username: 'velopert',
-        email: 'public.velopert@gmail.com',
-        active: true,
-      },
-      {
-        id: 2,
-        username: 'tester',
-        email: 'tester@example.com',
-        active: false,
-      },
-      {
-        id: 3,
-        username: 'liz',
-        email: 'liz@example.com',
-        active: false,
-      },
-    ],
-  };
-
-  const reducer = (state, action) => {
-    switch(action.type) {
-      // case 'CHANGE_INPUT':
-      //   return {
-      //     ...state,
-      //     inputs: {
-      //       ...state.inputs,
-      //       [action.name]: action.value,
-      //     },
-      //   };
-      case 'CREATE_USER':
-        return {
-          inputs: initialState.inputs,
-          users: state.users.concat(action.user),
-        };
-      case 'TOGGLE_USER':
-        return {
-          ...state,
-          users: state.users.map(user => 
-            user.id === action.id
-              ? { ...user, active: !user.active }
-              : user
-          ),
-        };
-      case 'REMOVE_USER':
-        return {
-          ...state,
-          users: state.users.filter(user =>
-            user.id !== action.id
-          )
-        };
-      default:
-        return state;
-    };
+const reducer = (state, action) => {
+  switch (action.type) {
+    // case 'CHANGE_INPUT':
+    //   return {
+    //     ...state,
+    //     inputs: {
+    //       ...state.inputs,
+    //       [action.name]: action.value,
+    //     },
+    //   };
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
+      return state;
   }
-
+};
 
 function App() {
   const [{ username, email }, onChange, reset] = useInputs({
-    username: '',
-    email: '',
+    username: "",
+    email: "",
   });
 
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -101,38 +95,43 @@ function App() {
 
   const onCreate = useCallback(() => {
     dispatch({
-      type: 'CREATE_USER',
+      type: "CREATE_USER",
       user: {
         id: nextId.current,
         username,
         email,
-      }
+      },
     });
     reset(); // 만든 후 input value 초기화 (useInputs 안에 있음)
-    nextId.current ++;
-  }, [username, email])
+    nextId.current++;
+  }, [username, email]);
 
-  const onToggle = useCallback(id => {
+  const onToggle = useCallback((id) => {
     dispatch({
-      type: 'TOGGLE_USER',
-      id
+      type: "TOGGLE_USER",
+      id,
     });
   }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
-  const onRemove = useCallback(id => {
+  const onRemove = useCallback((id) => {
     dispatch({
-      type: 'REMOVE_USER',
-      id
+      type: "REMOVE_USER",
+      id,
     });
-  }, [])
+  }, []);
 
   return (
     <>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
-      <div>활성사용자 수 : { count }</div>
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
--- TIL/2021-04-05/begin-react/src/Hooks/useInputs.js
+++ TIL/2021-04-05/begin-react/src/Hooks/useInputs.js
@@ -1,20 +1,19 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback } from "react";
 
 // initialForm은 App.js에서 실제 사용시 parameter로 받을 것
 const useInputs = (initialForm) => {
   const [form, setForm] = useState(initialForm);
 
-  const onChange = useCallback(e => {
+  const onChange = useCallback((e) => {
     const { name, value } = e.target;
-    setForm(form => ({
+    setForm((form) => ({
       ...form,
       [name]: value,
     }));
   }, []);
 
-  const reset = useCallback(() => setForm(initialForm), [initialForm])
-  ;
+  const reset = useCallback(() => setForm(initialForm), [initialForm]);
   return [form, onChange, reset];
-}
+};
 
 export default useInputs;
