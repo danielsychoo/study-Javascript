020641961cb1d0cc8a3c950c2a3ad2f402c4a60b
danielsychoo <choointern@gmail.com>
2021-04-07 16:34:53 +0900

    Update Nav CSS

--- TIL/2021-04-07/self-sprint/src/component/Ui/Nav/Nav.js
+++ TIL/2021-04-07/self-sprint/src/component/Ui/Nav/Nav.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Nav.scss";
 
+import { IconContext } from "react-icons";
+import { FcFilm, FcTrademark } from "react-icons/fc";
+import { BiSearch } from "react-icons/bi";
+
 const Nav = () => {
-  return <div className="Nav">This is Nav component</div>;
+  return (
+    <IconContext.Provider value={{ size: "2.2em" }}>
+      <div className="Nav">
+        <div className="navLogo">
+          <FcFilm />
+          MovieInfo
+        </div>
+        <div className="navRightBar">
+          <BiSearch />
+          <FcTrademark />
+        </div>
+      </div>
+    </IconContext.Provider>
+  );
 };
 
 export default Nav;
