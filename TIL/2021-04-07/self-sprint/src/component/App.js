bf1a2c52530299cdaba3a59fadaa91405aed39e9
danielsychoo <choointern@gmail.com>
2021-04-07 16:01:21 +0900

    Feature self-sprint

---
+++ TIL/2021-04-07/self-sprint/src/component/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+import Ui from "./Ui/Ui";
+
+function App() {
+  return (
+    <div className="App">
+      <Ui />
+    </div>
+  );
+}
+
+export default App;
---
+++ TIL/2021-04-07/self-sprint/src/component/Ui/Content/Content.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Content.scss";
+
+const Content = () => {
+  return <div className="Content">This is content</div>;
+};
+
+export default Content;
