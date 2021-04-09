5f5137becef769a4f10ea42eb3bc9922d42905ae
danielsychoo <choointern@gmail.com>
2021-04-09 16:09:46 +0900

    Update GraphQL Extending the Schema

--- apollo-graphql/graphql-movies/graphql/resolvers.js
+++ apollo-graphql/graphql-movies/graphql/resolvers.js
@@ -1,8 +1,15 @@
 // schema.graphql에서 define한 query를 resolve하는 역할
 
+// data sourse
+const daniel = {
+  name: "daniel",
+  age: 32,
+  gender: "male",
+};
+
 const resolvers = {
   Query: {
-    name: () => "daniel", // <쿼리>: <타입이 맞는 결과물 return>
+    person: () => daniel, // <쿼리>: <schema와 타입이 맞는 결과물 return>
   },
 };
 
