28eec00a5999d86757ef3fc38bc468df35abe0d8
danielsychoo <choointern@gmail.com>
2021-04-09 10:30:16 +0900

    Pause of Apollo-movie because of server

--- apollo-movies/src/routes/Home.js
+++ apollo-movies/src/routes/Home.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h1 key={movie.id}>{movie.id}</h1>);
+  }
+
   return <div>Home</div>;
 };
 
