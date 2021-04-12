a083d9bbaecbf929071617445af6fe94a516b979
danielsychoo <choointern@gmail.com>
2021-04-12 11:50:31 +0900

    Complete GraphQL server with Movie API

--- apollo-graphql/graphql-movies/graphql/resolvers.js
+++ apollo-graphql/graphql-movies/graphql/resolvers.js
@@ -1,18 +1,13 @@
 // schema.graphql에서 define한 query를 resolve하는 역할
 
 // data sourse
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => {
-      getById(id);
-    },
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
 
